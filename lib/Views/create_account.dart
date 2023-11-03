bcfefa7d547d646a50851ac4e697d7c07903b07b
SHAHMEER-ALYAN <shahmeeralyan1@gmail.com>
2023-11-04 03:23:44 +0500

    - Added Create Account

--- lib/Views/create_account.dart
+++ lib/Views/create_account.dart
@@ -1,23 +1,32 @@
+import 'dart:io';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:seizure_deck/database/create.dart';
+import 'package:http/http.dart' as http;
+import 'package:http/io_client.dart';
+
 
 class create_account extends StatefulWidget {
   const create_account({super.key});
+
 
   @override
   State<create_account> createState() => _create_accountState();
 }
 
-
 class _create_accountState extends State<create_account> {
 
-  DateTime selectedDate = DateTime.now(); 
+  DateTime selectedDate = DateTime.now(); // Initialize with the current date
 
   late TextEditingController name;
+  // name.text = "";
+
   late TextEditingController password;
+
   late TextEditingController DOB;
+
   late TextEditingController email;
+
   late TextEditingController location;
 
   @override
@@ -29,11 +38,12 @@ class _create_accountState extends State<create_account> {
     location = TextEditingController();
   }
 
+
   Future<void> _selectDate(BuildContext context) async {
     final DateTime? picked = await showDatePicker(
       context: context,
       initialDate: selectedDate,
-      firstDate: DateTime(1900), 
+      firstDate: DateTime(1900), // Adjust the range as needed
       lastDate: DateTime.now(),
     );
 
@@ -44,161 +54,49 @@ class _create_accountState extends State<create_account> {
     }
   }
 
-  ButtonStyle customElevatedButtonStyle = ElevatedButton.styleFrom(
-    foregroundColor: Colors.white, backgroundColor: const Color(0xFF454587), 
-    shape: RoundedRectangleBorder(
-      borderRadius: BorderRadius.circular(15.0),
-      side: const BorderSide(width: 5, color: Color(0xFF454587)),
-    ),
-  );
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                
-                const Text(
-                  "Enter Name",
-                  style: TextStyle(
-                      color: Color(0xFF454587),
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20),
-                ),
-                const SizedBox(height: 10,),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 15),
-                  child: TextField(
-                    controller: name,
-                    decoration: InputDecoration(
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(15.0),
-                        borderSide: const BorderSide(
-                            width: 5, color: Color(0xFF454587)),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(15.0),
-                          borderSide: const BorderSide(
-                              width: 5, color: Color(0xFF454587))),
-                        hintText: "Name"),
-                  ),
-                ),
-              const SizedBox(height: 20,),
-                const Text(
-                  "Enter Password",
-                  style: TextStyle(
-                      color: Color(0xFF454587),
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20),
-                ),
-                const SizedBox(height: 10,),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 15),
-                  child: TextField(
-                    controller: password,
-                    decoration: InputDecoration(
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(15.0),
-                        borderSide: const BorderSide(
-                            width: 5, color: Color(0xFF454587)),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(15.0),
-                          borderSide: const BorderSide(
-                              width: 5, color: Color(0xFF454587))),
-                        hintText: "Password"),
-                  ),
-                ),
-              const SizedBox(height: 20,),
-                const Text(
-                  "Enter Date of Birth",
-                  style: TextStyle(
-                      color: Color(0xFF454587),
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20),
-                ),
-                ElevatedButton(
-                    onPressed: () {
-                      _selectDate(context);
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              // `UID`, `Name`, `Password`, `DOB`, `Email`, `Location`, `PID`
+              Text("Enter Name"),
+              TextField(
+                controller: name,
+                decoration: InputDecoration(hintText: "Name"),
+              ),
+              Text("Enter Password"),
+              TextField(
+                controller: password,
+                decoration: InputDecoration(hintText: "Password"),
+              ),
+              Text("Enter Date of Birth"),
+              ElevatedButton(onPressed: () {
+                _selectDate(context);
+              } , child: Text("Date of Birth")),
+              Text("Enter Email"),
+              TextField(
+                controller: email,
+                decoration: InputDecoration(hintText: "Email"),
+              ),
+              Text("Enter Location"),
+              TextField(
+                controller: location,
+                decoration: InputDecoration(hintText: "Location"),
+              ),
+              ElevatedButton(onPressed: () {
+                print(location.text);
+                print(selectedDate);
+                print(name.text);
+                print(password.text);
+                print(email.text);
+                addUserToDatabase();
+              }, child: const Text("Create Account"))
 
-                    },
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color(0xFF454587)),
-                    child: const Text("Date of Birth",style: TextStyle(color: Colors.white),)),
-                const SizedBox(height: 20,),
-                const Text(
-                  "Enter Email",
-                  style: TextStyle(
-                      color: Color(0xFF454587),
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20),
-                ),
-                const SizedBox(height: 10,),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 15),
-                  child: TextField(
-                    controller: email,
-                    decoration: InputDecoration(
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(15.0),
-                        borderSide: const BorderSide(
-                            width: 5, color: Color(0xFF454587)),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(15.0),
-                          borderSide: const BorderSide(
-                              width: 5, color: Color(0xFF454587))),
-                        hintText: "Email"),
-                  ),
-                ),
-                const SizedBox(height: 20,),
-                const Text(
-                  "Enter Location",
-                  style: TextStyle(
-                      color: Color(0xFF454587),
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20),
-                ),
-                const SizedBox(height: 10,),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 15),
-                  child: TextField(
-                    controller: location,
-                    decoration: InputDecoration(
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(15.0),
-                        borderSide: const BorderSide(
-                            width: 5, color: Color(0xFF454587)),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(15.0),
-                          borderSide: const BorderSide(
-                              width: 5, color: Color(0xFF454587))),
-                        hintText: "Location"),
-                  ),
-                ),
-                const SizedBox(height: 10,),
-                ElevatedButton(
-                    onPressed: () {
-                      print(location.text);
-                      print("${selectedDate.toLocal()}".split(' ')[0]);
-                      print(name.text);
-                      print(password.text);
-                      print(email.text);
-                      addUserToDatabase(
-                          name, password, selectedDate, email, location);
-                      
-                    },
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color(0xFF454587)),
-                    child: const Text("Create Account",style: TextStyle(color: Colors.white),)
-                )
-              ],
-            ),
+            ],
           ),
         ),
       ),
@@ -206,27 +104,31 @@ class _create_accountState extends State<create_account> {
   }
 
 
+  Future<void> addUserToDatabase() async {
+    const url = 'https://10.0.2.2/seizure_deck_api/create.php'; // Replace with your server URL
 
+    final httpClient = HttpClient()..badCertificateCallback = (cert, host, port) => true;
 
+    final ioClient = IOClient(httpClient);
 
+    final response = await ioClient.post(
+      Uri.parse(url),
+      body: {
+        'UID': "", // You can generate a unique ID if needed
+        'Name': name.text,
+        'Password': password.text,
+        'DOB': "${selectedDate.toLocal()}".split(' ')[0], // Format the date
+        'Email': email.text,
+        'Location': location.text,
+        'PID': "", //
+      },
+    );
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    if (response.statusCode == 200) {
+      print("User added successfully!");
+    } else {
+      print("Failed to add user. Status code: ${response.statusCode}");
+    }
+  }
 }
+
--- lib/database/create.dart
+++ lib/database/create.dart
@@ -1,36 +1,27 @@
-import 'dart:io';
 
-import 'package:flutter/cupertino.dart';
-import 'package:http/io_client.dart';
+import 'package:http/http.dart' as http;
+import 'package:seizure_deck/Views/create_account.dart';
 
-Future<void> addUserToDatabase(
-    TextEditingController name,
-    TextEditingController password,
-    DateTime selectedDate,
-    TextEditingController email,
-    TextEditingController location) async {
-  const url =
-      'https://seizure-deck.000webhostapp.com/create.php'; // Replace with your server URL
 
-  final httpClient = HttpClient()
-    ..badCertificateCallback = (cert, host, port) => true;
-
-  final ioClient = IOClient(httpClient);
-
-  final response = await ioClient.post(
-    Uri.parse(url),
-    body: {
-      'name': name.text,
-      'email': email.text,
-      'password': password.text,
-      'dob': "${selectedDate.toLocal()}".split(' ')[0], // Format the date
-      'location': location.text,
-    },
-  );
-
-  if (response.statusCode == 200) {
-    print("User added successfully!");
-  } else {
-    print("Failed to add user. Status code: ${response.statusCode}");
-  }
-}
+// Future<void> addUserToDatabase() async {
+//   const url = 'https://localhost/seizure_deck_api/create.php'; // Replace with your server URL
+//
+//   final response = await http.post(
+//     Uri.parse(url),
+//     body: {
+//       'UID': name.value,
+//       'Name': 'your_name_value',
+//       'Password': password.value,
+//       'DOB': 'your_DOB_value',
+//       'Email': 'your_email_value',
+//       'Location': 'your_location_value',
+//       'PID': 'your_PID_value',
+//     },
+//   );
+//
+//   if (response.statusCode == 200) {
+//     print("User added successfully!");
+//   } else {
+//     print("Failed to add user. Status code: ${response.statusCode}");
+//   }
+// }

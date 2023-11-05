25115fa83478c8b9da1e41bfbb0e9d1b2ec5875a
SHAHMEER ALYAN <shahmeeralyan1@gmail.com>
2023-11-05 21:02:16 +0500

    -Improved Account Creation Back-End

--- lib/Views/create_account.dart
+++ lib/Views/create_account.dart
@@ -4,6 +4,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:http/io_client.dart';
+import 'package:seizure_deck/database/create.dart';
 
 
 class create_account extends StatefulWidget {
@@ -89,11 +90,12 @@ class _create_accountState extends State<create_account> {
               ),
               ElevatedButton(onPressed: () {
                 print(location.text);
-                print(selectedDate);
+                print("${selectedDate.toLocal()}".split(' ')[0]);
                 print(name.text);
                 print(password.text);
                 print(email.text);
-                addUserToDatabase();
+                addUserToDatabase(name, password, selectedDate, email, location);
+                // addUserToDatabase();
               }, child: const Text("Create Account"))
 
             ],
@@ -104,31 +106,29 @@ class _create_accountState extends State<create_account> {
   }
 
 
-  Future<void> addUserToDatabase() async {
-    const url = 'https://10.0.2.2/seizure_deck_api/create.php'; // Replace with your server URL
-
-    final httpClient = HttpClient()..badCertificateCallback = (cert, host, port) => true;
-
-    final ioClient = IOClient(httpClient);
-
-    final response = await ioClient.post(
-      Uri.parse(url),
-      body: {
-        'UID': "", // You can generate a unique ID if needed
-        'Name': name.text,
-        'Password': password.text,
-        'DOB': "${selectedDate.toLocal()}".split(' ')[0], // Format the date
-        'Email': email.text,
-        'Location': location.text,
-        'PID': "", //
-      },
-    );
-
-    if (response.statusCode == 200) {
-      print("User added successfully!");
-    } else {
-      print("Failed to add user. Status code: ${response.statusCode}");
-    }
-  }
+  // Future<void> addUserToDatabase() async {
+  //   const url = 'https://seizure-deck.000webhostapp.com/create.php'; // Replace with your server URL
+  //
+  //   final httpClient = HttpClient()..badCertificateCallback = (cert, host, port) => true;
+  //
+  //   final ioClient = IOClient(httpClient);
+  //
+  //   final response = await ioClient.post(
+  //     Uri.parse(url),
+  //     body: {
+  //       'Name': name.text,
+  //       'Password': password.text,
+  //       'DOB': "${selectedDate.toLocal()}".split(' ')[0], // Format the date
+  //       'Email': email.text,
+  //       'Location': location.text,
+  //     },
+  //   );
+  //
+  //   if (response.statusCode == 200) {
+  //     print("User added successfully!");
+  //   } else {
+  //     print("Failed to add user. Status code: ${response.statusCode}");
+  //   }
+  // }
 }
 
